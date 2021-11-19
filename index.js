ec76f630d47bf3195ab6c153175e6cf3328aa9b6
sunpengfei <rrlyrstar@163.com>
2021-11-19 17:53:50 +0800

    bug fix schema type

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ module.exports.schema2json = function (schema) {
     return JSON.stringify(_schema, function (key, value) {
         if (value instanceof RegExp) {
             return ('__REGEXP ' + value.toString());
+        } else if (value instanceof mongoose.Schema) {
+            return 'mongoose.Schema:' + module.exports.schema2json(value);
         } else if (typeof value === 'function') {
             if (key === 'validator') return value.toString();
             return value.name;
@@ -14,7 +16,23 @@ module.exports.schema2json = function (schema) {
 }
 
 module.exports.json2schema = function (json) {
-    const parsed = JSON.parse(json, function (key, value) {
+    const parsed = schemaJsonParse(json);
+    let schema = new mongoose.Schema(parsed.obj, parsed._userProvidedOptions)
+    parsed._indexes.map(item => schema.index(item[0], item[1]));
+    return schema;
+}
+
+
+function schemaJsonParse(json) {
+    return JSON.parse(json, function (key, value) {
+        if (typeof value === 'string' && value.startsWith('mongoose.Schema:')) {
+            let _v = value.substring(16);
+            try {
+                let _o = schemaJsonParse(_v);
+                if (_o) return module.exports.json2schema(_v);
+            } catch (e) {
+            }
+        }
         if (global.hasOwnProperty(value)) {
             return global[value];
         } else if (mongoose.Schema.Types.hasOwnProperty(value)) {
@@ -28,7 +46,4 @@ module.exports.json2schema = function (json) {
         }
         return value;
     })
-    let schema = new mongoose.Schema(parsed.obj, parsed._userProvidedOptions)
-    parsed._indexes.map(item => schema.index(item[0], item[1]));
-    return schema;
 }
